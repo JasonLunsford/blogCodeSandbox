8424e370a6b07dabe8f3b5213af99ccfebaf41e0
Jason Lunsford <miyamota@yahoo.com>
2014-09-19 17:34:38 -0400

    blog 2 sample code done

--- src/app/modusBlog02/modusBlog02.ctrl.js
+++ src/app/modusBlog02/modusBlog02.ctrl.js
@@ -1,6 +1,40 @@
 angular.module( 'blogCodeSandbox.modusBlog02.controllers', [] )
 	.controller( 'ModusBlog02Ctrl', function ModusBlog02Ctrl( $scope ) {
-		$scope.currentSection = "The Magic of Two Way Bindings: An Angular Deep Dive";
 
+	})
 
+	.controller( 'ExampleController', function ExampleController( $scope ) {
+		this.currentSection = "The Magic of Two Way Bindings: An Angular Deep Dive";
+		this.doubleCurl = "Double curly binding";
+		this.boundByBind = "ngBind binding";
+
+	})
+
+	.controller( 'NoDotController', function NoDotController( $scope ) {
+		// Empty!
+	})
+
+	.controller( 'DotController', function DotController( $scope ) {
+		// Empty!
+	})
+
+	.controller( 'FilterController', function FilterController( $scope ) {
+		this.nameList = [
+			{ name: 'Andrew' },
+			{ name: 'Luke' },
+			{ name: 'William' },
+			{ name: 'Jason' },
+			{ name: 'Skylar' },
+			{ name: 'Lucy' },
+			{ name: 'Zelda' }
+		];
+
+	})
+
+	.controller( 'FormatController', function FormatController( $scope ) {
+		this.submitMyForm = function(isValid) {
+			if (isValid) {
+				alert("Good Job!");
+			}
+		};
 	});
